0a43059a7687a459b78c35b22436d85c58683d5e
AurelieTorre <aurelie.torre@laposte.net>
2024-07-26 12:22:35 +0200

    navbar all-over

--- taxonomy-genre.php
+++ taxonomy-genre.php
@@ -2,7 +2,22 @@
 
 <?php get_header() ?>
 
-<h1 class="header__h1"><?php single_term_title(); ?></h1>
+<?php
+		$home_id = get_option('page_on_front'); // Récupérer l'ID de la page d'accueil
+		$image_url = get_field('home__header_wallpaper', $home_id); // Récupérer l'image de fond de la page d'accueil
+?>
+
+<header class="header" style="background-image:url('<?php echo $image_url; ?>'); background-position: center; "> <!-- echo sert à écrire l'url dans la valeur de l'attribut -->
+	<?php
+		wp_nav_menu(array(
+			'menu' => 'main-menu', // Name of the menu
+			'container' => 'nav',
+			'container_class' => 'header_menu',
+			'menu_class' => 'listemenu'
+		));
+	?>    
+	<h1 class="header__h1"><?php single_term_title(); ?></h1>
+</header>
 
 <div class="genre">
 	<?php if(have_posts()) : ?>
